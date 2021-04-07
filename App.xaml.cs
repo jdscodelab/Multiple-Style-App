fe5ca2581ad96c6533ce7528108ba5ecbc4f532e
Jd's Code Lab <jdscodelab@gmail.com>
2021-04-07 16:10:55 +0530

    Add files via upload
    - Initial Commit

---
+++ App.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Markup.Localizer;
+using MultipleStyleApp.Properties;
+
+namespace MultipleStyleApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        //Selecting first dictionary
+        private ResourceDictionary ThemeDictionary => Resources.MergedDictionaries[0];
+
+        public App()
+        {
+            InitializeComponent();
+
+            switch (Settings.Default.CurrentTheme)
+            {
+                case "Flat":
+                    //Change theme to Flat Style
+                    ChangeTheme(new Uri("/styles/flatstyle.xaml", UriKind.RelativeOrAbsolute));
+                //and save to settings
+                    Settings.Default.CurrentTheme = "Flat";
+                    Settings.Default.Save();
+                    break;
+
+                case "Modern":
+                    //Change theme to Flat Style
+                    ChangeTheme(new Uri("/styles/modernstyle.xaml", UriKind.RelativeOrAbsolute));
+                    //and save to settings
+                    Settings.Default.CurrentTheme = "Modern";
+                    Settings.Default.Save();
+                    break;
+
+                default:
+                    //Change theme to Flat Style
+                    ChangeTheme(new Uri("/styles/defaultstyle.xaml", UriKind.RelativeOrAbsolute));
+                    //and save to settings
+                    Settings.Default.CurrentTheme = "Default";
+                    Settings.Default.Save();
+                    break;
+            }
+        }
+
+        public void ChangeTheme(Uri uri)
+        {
+            ThemeDictionary.MergedDictionaries.Clear();
+            ThemeDictionary.MergedDictionaries.Add(new ResourceDictionary() {Source = uri});
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MultipleStyleApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!Properties.Settings.Default.CurrentTheme.Equals(ThemeSelectorCB.SelectedValue))
+            {
+                var app = (App) Application.Current;
+
+                switch (ThemeSelectorCB.SelectedIndex)
+                {
+                    case 1:
+                        //Change theme to Flat Style
+                        app.ChangeTheme(new Uri("/styles/flatstyle.xaml", UriKind.RelativeOrAbsolute));
+                        //and save to settings
+                        Properties.Settings.Default.CurrentTheme = "Flat";
+                        Properties.Settings.Default.Save();
+                        break;
+
+                    case 2:
+                        //Change theme to Flat Style
+                        app.ChangeTheme(new Uri("/styles/modernstyle.xaml", UriKind.RelativeOrAbsolute));
+                        //and save to settings
+                        Properties.Settings.Default.CurrentTheme = "Modern";
+                        Properties.Settings.Default.Save();
+                        break;
+
+                    default:
+                        //Change theme to Flat Style
+                        app.ChangeTheme(new Uri("/styles/defaultstyle.xaml", UriKind.RelativeOrAbsolute));
+                        //and save to settings
+                        Properties.Settings.Default.CurrentTheme = "Default";
+                        Properties.Settings.Default.Save();
+                        break;
+                }
+            }
+        }
+    }
+}
